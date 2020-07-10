8d90b96b5669763acc5d5a487fb4c5e219c09a59
Leafoliage <leafoliages@gmail.com>
2020-07-10 21:26:35 +0800

    [add] post schema

---
+++ model/post.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Schema.Types;
+
+const PostSchema = new mongoose.Schema({
+    id: { type: ObjectId },
+    author: { type: ObjectId },
+    title: { type: String },
+    content: { type: String },
+    likeId: { type: [ObjectId] },
+    dislikeId: { type: [ObjectId] },
+    comment: { type: [Object] }
+});
+
+module.exports = mongoose.model('Post', PostSchema);
