6de99f311cab9b363bec954c2662ee11ba8696aa
Leafoliage <leafoliages@gmail.com>
2020-07-12 22:25:24 +0800

    [modify] index

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const userAPI = require('./api/userAPI')
 const app = express()
+require('dotenv').config()
 
 app.use(express.json())
 
-// user=fire_tornado;pass=tornadoOnFire;db=firstMongo
-mongoose.connect('mongodb+srv://fire_tornado:tornadoOnFire@firstmongo-3qdnz.mongodb.net/CMSDcard?retryWrites=true&w=majority')
+const url = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@firstmongo-3qdnz.mongodb.net/${process.env.db_name}CMSDcard?retryWrites=true&w=majority`
+
+mongoose.connect(url)
 
 app.use(userAPI)
 
