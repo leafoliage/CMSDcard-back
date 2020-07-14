3e6c46219d3c122e6f4710356b629a6837e99e12
Leafoliage <leafoliages@gmail.com>
2020-07-14 16:58:01 +0800

    [modify] user schema

--- model/user.js
+++ model/user.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose')
 const { ObjectId } = mongoose.Schema.Types
 
 const UserSchema = new mongoose.Schema({
-    name: { type: String },
-    email: { type: String },
-    password: { type: String },
-    posts: { type: [ObjectId] }
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    // posts: { type: [ObjectId] }
 })
 
 module.exports = mongoose.model('User', UserSchema)
