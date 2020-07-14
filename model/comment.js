c4741012fa47d6ee11b2813932b221d1579d5950
Leafoliage <leafoliages@gmail.com>
2020-07-14 17:07:14 +0800

    [modify] comment schema

--- model/comment.js
+++ model/comment.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose')
 const { ObjectId } = mongoose.Schema.Types
 
 const CommentSchema = new mongoose.Schema({
-    author: { type: ObjectId },
-    targetPost: { type: ObjectId },
-    content: { type: String }
+    author: { type: ObjectId, required: true },
+    targetPost: { type: ObjectId, required: true },
+    content: { type: String, required: true },
+    postTime: { type: Date, set: new Date() }
 })
 
 module.exports = mongoose.model('Comment', CommentSchema)
