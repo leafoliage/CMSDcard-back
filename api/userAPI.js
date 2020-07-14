a9aa2320a806585f6cab9fe52374e163a52da731
Leafoliage <leafoliages@gmail.com>
2020-07-14 16:58:46 +0800

    [modify] userAPI

--- api/userAPI.js
+++ api/userAPI.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const { SHA256 } = require('crypto-js')
 const UserModel = require('../model/user')
 const api = express()
 
 api.get('/user/:id', async (req, res) => {
-    await UserModel.findById(req.params.id)
+    return await UserModel.findById(req.params.id)
         .then(data => { res.send(data) })
         .catch(err => { res.status(500).send(err) })
 })
@@ -11,19 +12,27 @@ api.get('/user/:id', async (req, res) => {
 api.post('/user', async (req, res) => {
     const user = new UserModel(req.body)
 
-    await user.save()
+    if (user.password) {
+        user.password = SHA256(user.password)
+    }
+
+    return await user.save()
         .then(data => { res.send(data) })
         .catch(err => { res.status(500).send(err) })
 })
 
 api.put('/user/:id', async (req, res) => {
-    await UserModel.findByIdAndUpdate(req.params.id, req.body)
+    if (req.body.password) {
+        req.body.password = SHA256(req.body.password)
+    }
+
+    return await UserModel.findByIdAndUpdate(req.params.id, req.body)
         .then(data => { res.send(data) })
         .catch(err => { res.status(500).send(err) })
 })
 
 api.delete('/user/:id', async (req, res) => {
-    await UserModel.findByIdAndDelete(req.params.id)
+    return await UserModel.findByIdAndDelete(req.params.id)
         .then(data => { res.send(data) })
         .catch(err => { res.status(500).send(err) })
 })
