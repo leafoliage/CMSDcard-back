09056c34aecd4455be13ba8dfaa35d38cf83458e
Leafoliage <leafoliages@gmail.com>
2020-08-03 21:53:11 +0800

    [add] add token authentication to handlers

--- api/commentAPI.js
+++ api/commentAPI.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const CommentModel = require('../model/comment')
 const UserModel = require('../model/user')
+const authenticateToken = require('./auth')
 const api = express()
 
 api.get('/comment/single/:id', async (req, res) => {
@@ -9,20 +10,20 @@ api.get('/comment/single/:id', async (req, res) => {
         .catch(err => { res.status(500).send(err) })
 })
 
-api.get('/comment/post/:postId', async (req, res) => {
+api.get('/comment/post/:postId', authenticateToken, async (req, res) => {
     return await CommentModel.find({ targetPost: req.params.postId })
         .then(data => { res.send(data) })
         .catch(err => { res.status(500).send(err) })
 })
 
-api.post('/comment', async (req, res) => {
-    const user = await UserModel.findById(req.body.authorId)
+api.post('/comment/:postId', authenticateToken, async (req, res) => {
+    const user = await UserModel.findById(req.currUser.userId)
         .catch(err => { res.status(500).send(err) })
 
     let comment = {
-        authorId: req.body.authorId,
+        authorId: req.currUser.userId,
         authorName: user.name,
-        targetPost: req.body.targetPost,
+        targetPost: req.params.postId,
         content: req.body.content,
     }
 
--- api/postAPI.js
+++ api/postAPI.js
@@ -1,32 +1,33 @@
 const express = require('express')
 const PostModel = require('../model/post')
 const UserModel = require('../model/user')
+const authenticateToken = require('./auth')
 const api = express()
 
-api.get('/post/:id', async (req, res) => {
+api.get('/post/:id', authenticateToken, async (req, res) => {
     return await PostModel.findById(req.params.id)
         .then(data => { res.send(data) })
         .catch(err => { res.status(500).send(err) })
 })
 
-api.get('/post/select/hot', async (req, res) => {
+api.get('/post/select/hot', authenticateToken, async (req, res) => {
     return await PostModel.find().sort({ likeNum: -1 }).limit(5)
         .then(data => { res.send(data) })
         .catch(err => { res.status(500).send(err) })
 })
 
-api.get('/post/select/new', async (req, res) => {
+api.get('/post/select/new', authenticateToken, async (req, res) => {
     return await PostModel.find().sort({ postTime: -1 }).limit(10)
         .then(data => { res.send(data) })
         .catch(err => { res.status(500).send(err) })
 })
 
-api.post('/post', async (req, res) => {
-    const user = await UserModel.findById(req.body.authorId)
+api.post('/post', authenticateToken, async (req, res) => {
+    const user = await UserModel.findById(req.currUser.userId)
         .catch(err => { res.status(500).send(err) })
 
     let post = {
-        authorId: req.body.authorId,
+        authorId: req.currUser.userId,
         authorName: user.name,
         title: req.body.title,
         content: req.body.content,
@@ -45,9 +46,8 @@ api.put('/post/:id', async (req, res) => {
         .catch(err => { res.status(500).send(err) })
 })
 
-api.put('/post/like/:postId', async (req, res) => {
-
-    return await PostModel.findByIdAndUpdate(req.params.postId, { $push: { likeIds: req.body.userId }, $inc: { likeNum: 1 } })
+api.put('/post/like/:postId', authenticateToken, async (req, res) => {
+    return await PostModel.findByIdAndUpdate(req.params.postId, { $push: { likeIds: req.currUser.userId }, $inc: { likeNum: 1 } })
         .then(data => { res.send(data) })
         .catch(err => { res.status(500).send(err) })
 })
