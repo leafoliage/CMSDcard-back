6a8835500e56819b0fe297d68f0216948d8de9e6
Leafoliage <leafoliages@gmail.com>
2020-08-03 18:10:57 +0800

    [add] login handler

--- api/userAPI.js
+++ api/userAPI.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
-const UserModel = require('../model/user')
 const api = express()
+const jwt = require('jsonwebtoken')
+const UserModel = require('../model/user')
 
 api.get('/user/:id', async (req, res) => {
     try {
@@ -12,11 +13,11 @@ api.get('/user/:id', async (req, res) => {
 
         res.status(200).send(user)
     } catch (err) {
-        res.status(500).send(err)
+        res.status(500).send(err.message)
     }
 })
 
-api.post('/user', async (req, res) => {
+api.post('/user/register', async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
 
@@ -29,7 +30,26 @@ api.post('/user', async (req, res) => {
         const data = await UserModel.create(user)
         res.status(201).send(data)
     } catch (err) {
-        res.status(500).send(err)
+        res.status(500).send(err.message)
+    }
+})
+
+api.post('/user/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email })
+        if (!user) {
+            res.status(404).send('user not found')
+        }
+
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            const data = { userId: user._id }
+            const accessToken = jwt.sign(data, process.env.access_token_secret)
+            res.json({ accessToken: accessToken })
+        } else {
+            res.status(401).send('Login Fail')
+        }
+    } catch (err) {
+        res.status(500).send(err.message)
     }
 })
 
@@ -47,14 +67,14 @@ api.put('/user/:id', async (req, res) => {
         const data = await UserModel.findById(req.params.id)
         res.status(200).send(data)
     } catch (err) {
-        res.status(500).send(err)
+        res.status(500).send(err.message)
     }
 })
 
 // api.delete('/user/:id', async (req, res) => {
 //     return await UserModel.findByIdAndDelete(req.params.id)
 //         .then(data => { res.send(data) })
-//         .catch(err => { res.status(500).send(err) })
+//         .catch(err => { res.status(500).send(err.message) })
 // })
 
 module.exports = api
