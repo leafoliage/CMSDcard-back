2844d7064319bbdee0ebb3eaf8cf46e469d493c0
Leafoliage <leafoliages@gmail.com>
2020-08-04 08:51:23 +0800

    [modify] userAPI handlers add return

--- api/userAPI.js
+++ api/userAPI.js
@@ -11,9 +11,9 @@ api.get('/user/:id', async (req, res) => {
             res.status(404).send('User not found')
         }
 
-        res.status(200).send(user)
+        return res.status(200).send(user)
     } catch (err) {
-        res.status(500).send(err.message)
+        return res.status(500).send(err.message)
     }
 })
 
@@ -36,9 +36,9 @@ api.post('/user/register', async (req, res) => {
         }
 
         const data = await UserModel.create(user)
-        res.status(201).send(data)
+        return res.status(201).send(data)
     } catch (err) {
-        res.status(500).send(err.message)
+        return res.status(500).send(err.message)
     }
 })
 
@@ -46,18 +46,18 @@ api.post('/user/login', async (req, res) => {
     try {
         const user = await UserModel.findOne({ email: req.body.email })
         if (!user) {
-            res.status(404).send('user not found')
+            return res.status(404).send('user not found')
         }
 
         if (await bcrypt.compare(req.body.password, user.password)) {
             const data = { userId: user._id }
             const accessToken = jwt.sign(data, process.env.access_token_secret)
-            res.json({ accessToken: accessToken })
+            return res.json({ accessToken: accessToken })
         } else {
-            res.status(401).send('Login Fail')
+            return res.status(401).send('Login Fail')
         }
     } catch (err) {
-        res.status(500).send(err.message)
+        return res.status(500).send(err.message)
     }
 })
 
@@ -69,20 +69,20 @@ api.put('/user/:id', async (req, res) => {
 
         const user = await UserModel.findByIdAndUpdate(req.params.id, req.body)
         if (!user) {
-            res.status(404).send('User not found')
+            return res.status(404).send('User not found')
         }
 
         const data = await UserModel.findById(req.params.id)
-        res.status(200).send(data)
+        return res.status(200).send(data)
     } catch (err) {
-        res.status(500).send(err.message)
+        return res.status(500).send(err.message)
     }
 })
 
-// api.delete('/user/:id', async (req, res) => {
-//     return await UserModel.findByIdAndDelete(req.params.id)
-//         .then(data => { res.send(data) })
-//         .catch(err => { res.status(500).send(err.message) })
-// })
+api.delete('/user/:id', async (req, res) => {
+    return await UserModel.findByIdAndDelete(req.params.id)
+        .then(data => { res.send(data) })
+        .catch(err => { res.status(500).send(err.message) })
+})
 
 module.exports = api
