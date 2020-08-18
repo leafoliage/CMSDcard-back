f93f5fbb99a6a4df3713348b7565439f4c267eea
Leafoliage <leafoliages@gmail.com>
2020-08-18 22:20:33 +0800

    [modify] comment handler rework

--- api/commentAPI.js
+++ api/commentAPI.js
@@ -5,44 +5,62 @@ const authenticateToken = require('./auth')
 const api = express()
 
 api.get('/comment/single/:id', async (req, res) => {
-    return await CommentModel.findById(req.params.id)
-        .then(data => { res.send(data) })
-        .catch(err => { res.status(500).send(err) })
+    try {
+        const data = await CommentModel.findById(req.params.id)
+        if (!data) res.sendStatus(404)
+        return res.status(200).send(data)
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
 })
 
 api.get('/comment/post/:postId', authenticateToken, async (req, res) => {
-    return await CommentModel.find({ targetPost: req.params.postId })
-        .then(data => { res.send(data) })
-        .catch(err => { res.status(500).send(err) })
+    try {
+        const data = await CommentModel.find({ targetPost: req.params.postId })
+        if (!data) res.sendStatus(404)
+        return res.status(200).send(data)
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
 })
 
 api.post('/comment/:postId', authenticateToken, async (req, res) => {
-    const user = await UserModel.findById(req.currUser.userId)
-        .catch(err => { res.status(500).send(err) })
+    try {
+        const user = await UserModel.findById(req.currUser.userId)
 
-    let comment = {
-        authorId: req.currUser.userId,
-        authorName: user.name,
-        targetPost: req.params.postId,
-        content: req.body.content,
-        postTime: new Date()
+        let comment = {
+            authorId: req.currUser.userId,
+            authorName: user.name,
+            targetPost: req.params.postId,
+            content: req.body.content,
+            postTime: new Date()
+        }
+
+        const data = await CommentModel.create(comment)
+        return res.status(200).send(data)
+    } catch (err) {
+        return res.status(500).send(err.message)
     }
-
-    return await CommentModel.create(comment)
-        .then(data => { res.send(data) })
-        .catch(err => { res.status(500).send(err) })
 })
 
 api.put('/comment/:id', async (req, res) => {
-    return await CommentModel.findByIdAndUpdate(req.params.id, req.body)
-        .then(data => { res.send(data) })
-        .catch(err => { res.status(500).send(err) })
+    try {
+        const data = await CommentModel.findByIdAndUpdate(req.params.id, req.body)
+        if (!data) res.sendStatus(404)
+        return res.status(200).send(data)
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
 })
 
 api.delete('/comment/:id', async (req, res) => {
-    return await CommentModel.findByIdAndDelete(req.params.id)
-        .then(data => { res.send(data) })
-        .catch(err => { res.status(500).send(err) })
+    try {
+        const data = await CommentModel.findByIdAndDelete(req.params.id)
+        if (!data) res.sendStatus(404)
+        return res.status(200).send(data)
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
 })
 
 module.exports = api
