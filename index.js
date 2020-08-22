985090590f9ca100403152a9c877cfa1a7453bad
Leafoliage <leafoliages@gmail.com>
2020-08-22 11:04:50 +0800

    [modify] update db

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const commentAPI = require('./api/commentAPI')
 app.use(express.json())
 app.use(cors())
 
-const url = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@firstmongo-3qdnz.mongodb.net/${process.env.db_name}?retryWrites=true&w=majority`
+const url = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@forumcluster.3qdnz.gcp.mongodb.net/${process.env.db_name}?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 
